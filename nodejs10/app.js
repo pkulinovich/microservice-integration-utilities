515006d5c505e5e284f071b454419da2cf1c999b
Pavel <paulk@velmie.com>
2019-07-01 02:35:45 +0300

    fixes

--- nodejs10/app.js
+++ nodejs10/app.js
@@ -3,7 +3,7 @@ const app = express()
 const sls = require('serverless-http')
 
 const errorHandler = require('./middlewares/errors');
-//const NotFoundError = require('./errors/NotFoundError');
+const NotFoundError = require('./errors/NotFoundError');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,13 +14,13 @@ app.use('/xmltojson', require('./routes/xmltojson'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.sendStatus(404)
+  throw new NotFoundError()
 });
 
 // error handler
 app.use(errorHandler);
 
 // start server
-//app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 
-module.exports.server = sls(app)
+//module.exports.server = sls(app)
--- nodejs10/middlewares/errors.js
+++ nodejs10/middlewares/errors.js
@@ -1,4 +1,4 @@
-const AppError = require('../errors/appError');
+const AppError = require('../errors/AppError');
 
 /**
  * Error handler.
